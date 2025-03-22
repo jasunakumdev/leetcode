1e34cf4aca439be69dabbf1e53d810f6cd78c956
Jasu Nakum <jasunakum@Jasus-MacBook-Pro.local>
2025-03-22 15:49:13 +0530

    Easy 66: Plus One

---
+++ Easy/plusOne-66.js
@@ -0,0 +1,15 @@
+var plusOne = function (digits) {
+  const n = digits.length
+  for (let i = n - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++
+      return digits
+    }
+    digits[i] = 0
+    console.log(digits)
+  }
+  digits.unshift(1)
+  return digits
+}
+
+console.log(plusOne([9]))
